558afe3529db4e372c304c62ba36676d754b4749
nirajsathe80 <nirajsathe80@gmail.com>
2024-09-11 19:34:41 +0530

    tab navigation section add

---
+++ src/components/tab-navigation/index.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from "../data/index";
+import { useGlobalContext } from "../../context/global/global-context-hook";
+
+const TabNavigation = () => {
+  const { currentTab, updateCurrentTab } = useGlobalContext();
+  return (
+    <nav className="xsm:text-sm text-[10px] font-medium text-center text-gray-500 border-b border-gray-200  ">
+      <ul className="flex flex-wrap -mb-px">
+        {Tabs.map((tab) => {
+          const { label, value } = tab;
+          return (
+            <li className="me-2" key={value}>
+              <div
+                className={`inline-block sm:p-4 p-1 border-b-2 rounded-t-lg  cursor-pointer ${
+                  value === currentTab ? "text-black border-blue-400 z-10 " : ""
+                } hover:text-blue-300  `}
+                onClick={() => updateCurrentTab(value)}
+              >
+                {label}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default TabNavigation;
