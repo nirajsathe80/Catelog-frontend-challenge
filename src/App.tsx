2c5b0ab3afc901b95558d9f68307474011b28dfe
nirajsathe80 <nirajsathe80@gmail.com>
2024-09-11 19:36:20 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Homepage from "./pages/homepage";
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-[#141619] flex justify-center items-center">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
