4be677f95a8986c3e03728d806ca14d83b88e32c
nirajsathe80 <nirajsathe80@gmail.com>
2024-09-11 19:30:34 +0530

    data file create

---
+++ src/components/data/index.ts
@@ -0,0 +1,24 @@
+export const Tabs = [
+  {
+    label: "Summary",
+    value: "summary",
+  },
+  {
+    label: "Chart",
+    value: "chart",
+  },
+  {
+    label: "Statistics",
+    value: "statistics",
+  },
+  {
+    label: "Analysis",
+    value: "analysis",
+  },
+  {
+    label: "Settings",
+    value: "settings",
+  },
+];
+
+export const periods = ["1w", "1m", "6m", "1y", "max"];
