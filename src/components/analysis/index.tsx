02e26aded45f0e48e69d5e48c533e2364a49a4de
nirajsathe80 <nirajsathe80@gmail.com>
2024-09-11 19:34:26 +0530

    analysis section code add

---
+++ src/components/analysis/index.tsx
@@ -0,0 +1,9 @@
+const Analysis = function () {
+  return (
+    <div className="text-base font-medium flex justify-center my-10">
+      Soon will be developing this section
+    </div>
+  );
+};
+
+export default Analysis;
